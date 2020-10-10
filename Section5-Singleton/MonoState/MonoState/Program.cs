651feb0b7ca31f7a33553a417fdf4a2ca28259b2
Jesus Leon <jesusleon@outlook.com>
2020-10-10 18:00:12 -0300

    Singleton Monostate

---
+++ Section5-Singleton/MonoState/MonoState/Program.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using static System.Console;
+
+namespace MonoState
+{
+    public class ChiefExecutiveOfficer
+    {
+        private static string name;
+        private static int age;
+
+        public string Name
+        {
+            get => name;
+            set => name = value;
+        }
+
+        public int Age
+        {
+            get => age;
+            set => age = value;
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Name)}: {Name}, {nameof(Age)}: {Age}";
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ceo = new ChiefExecutiveOfficer();
+            ceo.Name = "Adam Smith";
+            ceo.Age = 55;
+
+            var ceo2 = new ChiefExecutiveOfficer();
+            WriteLine(ceo2);
+            Console.ReadKey();
+
+        }
+    }
+}
